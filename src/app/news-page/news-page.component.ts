d12412f11962650720e635e9f20c5d14035762d5
JoshGoodman22 <jgoodman2024@chadwickschool.org>
2022-07-21 19:41:05 -0700

    Added a Sign-In Option to create an Account.Adstf

--- src/app/news-page/news-page.component.ts
+++ src/app/news-page/news-page.component.ts
@@ -1,5 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
+interface NewsSecData {
+  title: string;
+  mainImgDec: string;
+  image: string;
+  Subheadline1: string;
+  Subheadline2: string;
+  Subheadline3: string;
+}
+
 @Component({
   selector: 'app-news-page',
   templateUrl: './news-page.component.html',
@@ -7,9 +16,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewsPageComponent implements OnInit {
 
+  data: NewsSecData[] = [
+    { 
+      title: "Trending",
+      mainImgDec: "Man was found alive hours before he was found dead",
+      image: "CrimeEx.jpg",
+      Subheadline1: "(BBC) Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi at.",
+      Subheadline2: "(AP) Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi at.",
+      Subheadline3: "(AP) Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi at."
+    },
+    { 
+      title: "Politics",
+      mainImgDec: "Boris Johnson has trouble in class",
+      image: "BorisSample.jpg",
+      Subheadline1: "(BBC) Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi at.",
+      Subheadline2: "(AP) Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi at.",
+      Subheadline3: "(AP) Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi at."
+    }
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  addNewSection(): void {
+    let newSec = { 
+      title: "Politics",
+      mainImgDec: "Boris Johnson has trouble in class",
+      image: "BorisSample.jpg",
+      Subheadline1: "(BBC) Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi at.",
+      Subheadline2: "(AP) Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi at.",
+      Subheadline3: "(AP) Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi at."
+    };
+    this.data.push(newSec);
   }
 
 }
