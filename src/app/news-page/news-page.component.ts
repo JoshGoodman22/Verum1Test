5d7fcadc729021527c58aed23d41d110b8948aeb
JoshGoodman22 <jgoodman2024@chadwickschool.org>
2022-07-26 11:20:24 -0700

    Added new service - added packages for XML JSON

--- src/app/news-page/news-page.component.ts
+++ src/app/news-page/news-page.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import * as xml2js from 'xml2js';
+
+interface NewsData {
+  news: NewsSecData[];
+}
 
 interface NewsSecData {
   title: string;
@@ -16,40 +22,39 @@ interface NewsSecData {
 })
 export class NewsPageComponent implements OnInit {
 
-  data: NewsSecData[] = [
-    { 
-      title: "Trending",
-      mainImgDec: "Man was found alive hours before he was found dead",
-      image: "CrimeEx.jpg",
-      Subheadline1: "(BBC) Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi at.",
-      Subheadline2: "(AP) Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi at.",
-      Subheadline3: "(AP) Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi at."
-    },
-    { 
-      title: "Politics",
-      mainImgDec: "Boris Johnson has trouble in class",
-      image: "BorisSample.jpg",
-      Subheadline1: "(BBC) Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi at.",
-      Subheadline2: "(AP) Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi at.",
-      Subheadline3: "(AP) Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi at."
-    }
-  ];
+  dataUrl: string = "https://raw.githubusercontent.com/JoshGoodman22/Verum1Test/master/src/assets/news.json";
+  bbcRss: string = "https://raw.githubusercontent.com/JoshGoodman22/Verum1Test/master/src/assets/bbcrss.xml";
+  data: NewsSecData[] = [];
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
+    this.http.get<NewsData>(this.dataUrl).subscribe((data: NewsData) => {
+      // this.data = data.news;
+    });
+    this.http.get(this.bbcRss, { responseType: "text" }).subscribe((data) => {
+      const parser = new xml2js.Parser({ strict: false, trim: true });
+      parser.parseString(data, (err, result) => {
+        // console.log(result);
+        console.log(result.RSS.CHANNEL[0].ITEM);
+        // This will actually be a loop here not "3"
+        let item = result.RSS.CHANNEL[0].ITEM[2]; 
+        // TODO: Write a loop over result.RSS.CHANNEL[0].ITEM
+        // for each ITEM, you need to extract Data
+        // such that you can create a NewsSecData
+        let row: NewsSecData = {
+          title: item.TITLE[0],
+          mainImgDec: "",
+          image: "",
+          Subheadline1: "",
+          Subheadline2: "",
+          Subheadline3: ""
+        }
+        this.data.push(row);
+
+      });
+    });
   }
 
-  addNewSection(): void {
-    let newSec = { 
-      title: "Politics",
-      mainImgDec: "Boris Johnson has trouble in class",
-      image: "BorisSample.jpg",
-      Subheadline1: "(BBC) Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi at.",
-      Subheadline2: "(AP) Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi at.",
-      Subheadline3: "(AP) Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi at."
-    };
-    this.data.push(newSec);
-  }
 
 }
