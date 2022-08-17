20a12e19c1306843c76c49853a384d0a571a53f8
JoshGoodman22 <jgoodman2024@chadwickschool.org>
2022-08-17 13:14:10 -0700

    added all new climate rss feeds

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { HeaderComponent } from './header/header.component';
 import { NewsSecComponent } from './news-sec/news-sec.component';
 import { NewsPageComponent } from './news-page/news-page.component';
 import { SignInPageComponent } from './sign-in-page/sign-in-page.component';
+import { ModalComponent } from './modal/modal.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { SignInPageComponent } from './sign-in-page/sign-in-page.component';
     HeaderComponent,
     NewsSecComponent,
     NewsPageComponent,
-    SignInPageComponent
+    SignInPageComponent,
+    ModalComponent
   ],
   imports: [
     BrowserModule,
